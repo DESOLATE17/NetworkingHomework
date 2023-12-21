e811a68ce3ac0e761534b9386cb17d3ec99fd907
DESOLATE17 <taktashovadasha@yandex.ru>
2023-12-21 15:56:44 +0300

    added c(7,4) with long polling

--- long_polling/frontend/index.js
+++ long_polling/frontend/index.js
@@ -1,59 +1,167 @@
-const list = document.getElementById('list'); // список ответов от сервера
-const startButton = document.getElementById('start'); // кнопка запуска long polling запросов
-const finishButton = document.getElementById('finish'); // кнопка завершения long polling запросов
+const list = document.getElementById("list"); // список ответов от сервера
+const startButton = document.getElementById("start"); // кнопка запуска long polling запросов
+const finishButton = document.getElementById("finish"); // кнопка завершения long polling запросов
 let isPolling = false; // текущее состояние запросов
 
-const subscribe = async () => {
-  try {
-    // отправляем fetch запрос на эндпоинт
-    const response = await fetch('/long-polling-request');
-    // создаем новый узел li
-    const node = document.createElement('li');
+const generatingPolynomial = "1011";
+let verificationMatrix = ["1", "10", "100", "11", "110", "111", "101"];
 
-    // если статус 200 (все хорошо)
-    if (response.status === 200) {
-      // Забираем данные из запроса
-      const data = await response.json();
+let corruptedPolynomial;
+let encodedPolynomial;
+let errorCount; 
+let originalPolynomial;
 
-      // заполняем узел li содержимым
-      node.innerHTML
-        = `Данное сообщение с числом <span class="bold">${data.value}</span> было получено через <span class="bold">${data.timeout}с</span> после начала long polling запроса`
-    } else if (response.status === 502) { // если время ожидания ответа превышено
-      node.innerText = 'Превышено время ожидания ответа от сервера (> 7с)';
-    }
+const decoding = (data) => {
+	({corruptedPolynomial, encodedPolynomial, errorCount, originalPolynomial} = data);
 
-    // вставляем в список новый узел
-    list.appendChild(node);
+	const remainder = getDividingRemainder(corruptedPolynomial);
 
-    // если соединение еще не прервано, то рекурсивно запускаем функцию subscribe
-    if (isPolling) {
-      subscribe();
-    }
-  } catch (e) {
-    // если в процессе запроса возникла непредвиденная ошибка на сервере, то запускаем функцию через 1с
-    setTimeout(() => {
-      // если соединение еще не прервано, то рекурсивно запускаем функцию subscribe
-      if (isPolling) {
-        subscribe()
-      }
-    }, 1000)
-  }
+	let index = verificationMatrix.findIndex(element => element === remainder);
+
+	let decodedPolynomial = corruptedPolynomial.slice(0, corruptedPolynomial.length - 3);
+
+	if (index < 0) {
+		if (originalPolynomial === decodedPolynomial) {
+			return message(originalPolynomial,
+			 encodedPolynomial, corruptedPolynomial, decodedPolynomial, errorCount,
+			 1);
+		} else {
+			return message(originalPolynomial,
+			 encodedPolynomial, corruptedPolynomial, decodedPolynomial, errorCount,
+			 4);
+		}
+	} else {
+		index = corruptedPolynomial.length - index - 1;
+
+		let correctedPolynomial = corruptedPolynomial.substring(0, index) + String(corruptedPolynomial[index] ^ 1) + 
+				corruptedPolynomial.substring(index + 1);
+
+		decodedPolynomial = correctedPolynomial.slice(0, corruptedPolynomial.length - 3);
+
+		if (originalPolynomial === decodedPolynomial) {
+			return message(originalPolynomial,
+			 encodedPolynomial, corruptedPolynomial, decodedPolynomial, errorCount,
+			 2);
+		} else {
+			return message(originalPolynomial,
+			 encodedPolynomial, corruptedPolynomial, decodedPolynomial, errorCount,
+			 3)
+		}
+	}
 }
+
+const getDividingRemainder = (polynomial) => {
+	let indexEnd = generatingPolynomial.length - 1;
+	let currentDigit = polynomial.slice(0, indexEnd + 1);
+	let remainder;
+
+	while (indexEnd < polynomial.length) {
+		remainder = (parseInt(currentDigit, 2) ^ parseInt(generatingPolynomial, 2)).toString(2);
+		currentDigit = remainder;
+
+		if ((++indexEnd) < polynomial.length) {
+			while (indexEnd < polynomial.length && currentDigit.length < generatingPolynomial.length) {
+				currentDigit += polynomial[indexEnd++];
+				currentDigit = String(+currentDigit);
+			}
+			
+			if (currentDigit.length < generatingPolynomial.length) {
+				remainder = currentDigit;
+			} else {
+				indexEnd--;
+			}
+		}
+	}
+	
+	return remainder;
+}
+
+const subscribe = async () => {
+	try {
+		const response = await fetch("/long-polling-request");
+
+		const node = document.createElement("div");
+		node.style.marginBottom = "1em";
+
+		if (response.status === 200) {
+			const data = await response.json();
+			node.innerHTML = decoding(data);
+		} else if (response.status === 502) {
+			node.innerText = `Превышено время ожидания ответа от сервера`;
+		}
+
+		list.appendChild(node);
+
+		// если соединение еще не прервано, то рекурсивно запускаем функцию subscribe
+		if (isPolling) {
+			subscribe();
+		}
+	} catch (e) {
+		// если в процессе запроса возникла непредвиденная ошибка на сервере, то запускаем функцию через 1с
+		setTimeout(() => {
+			// если соединение еще не прервано, то рекурсивно запускаем функцию subscribe
+			if (isPolling) {
+				subscribe();
+			}
+		}, 1000);
+	}
+};
 
 // функция вызывается при нажатии на кнопку "начать"
 const startConnectToServer = () => {
-  console.log(finishButton)
-  console.log(startButton)
-  finishButton.disabled = false;
-  startButton.disabled = true;
-  isPolling = true;
+	finishButton.disabled = false;
+	startButton.disabled = true;
+	isPolling = true;
 
-  subscribe()
-}
+	subscribe();
+};
 
 // функция вызывается при нажатии на кнопку "закончить"
 const finishConnectToServer = () => {
-  startButton.disabled = false;
-  finishButton.disabled = true;
-  isPolling = false;
-}
+	startButton.disabled = false;
+	finishButton.disabled = true;
+	isPolling = false;
+};
+
+startButton.addEventListener("click", startConnectToServer);
+finishButton.addEventListener("click", finishConnectToServer);
+
+
+const statusList = {
+	1: "Ошибки не было",
+	2: "Ошибка была, получилось исправить",
+	3: "Ошибка была, не смогли исправить",
+	4: "Ошибка была, не нашли",
+  }
+  
+const message = (original, encoded, corrupted, decoded, errorCount, status) => {
+	  return `
+  <div class="message message-${status}">
+	<div class="errors_count">Количество ошибок: ${errorCount}.  ${statusList[status]}</div>
+	
+	<div class="comparison">
+	  <div>
+		<div class="comparison__message corrupted">
+		  <div class="title">Полученное сообщение: </div>
+		  <div class="content">${corrupted}</div>
+		</div>
+		<div class="comparison__message encoded">
+		  <div class="title">Отправленное сообщение: </div>
+		  <div class="content">${encoded}</div>
+		</div>
+	  </div>
+  
+	  <div>
+		<div class="comparison__message decoded">
+		  <div class="title">Раскодированное сообщение: </div>
+		  <div class="content">${decoded}</div>
+		</div>
+		<div class="comparison__message original">
+		  <div class="title">Оригинальное сообщение: </div>
+		  <div class="content">${original}</div>
+		</div>
+	  </div>
+	</div>
+  </div>
+	  `;
+  }
--- long_polling/index.js
+++ long_polling/index.js
@@ -1,42 +1,148 @@
-const express = require('express'); // импорт библиотеки express
-const path = require('path'); // импорт библиотеки path для работы с путями
-const app = express(); // создание экземпляра приложения express
-const PORT = process.env.PORT || 3000; // присвоения порта
-const MAX_RESPONSE_TIMEOUT = 7000; // максимальное время ответа от сервера
+const express = require("express"); // импорт библиотеки express
+const path = require("path"); // импорт библиотеки path для работы с путями
 
+const app = express(); // создание экземпляра приложения express
+const PORT = 3000; // присвоения порта
+
+const MAX_RESPONSE_TIMEOUT = 4000;
+const MAX_TIMEOUT = 5000; // максимальное время ожидания ответа
+const MIN_TIMEOUT = 1000; // минимальное время ожидания
+const MAX_VALUE = 15; // максимальное значение случайного числа
+const MIN_VALUE = 1; // минимальное значение случайного числа
+const ENCODED_POLY_LEN = 7;
+const POLY_LEN = 4;
+
+// порождающий полином для c(7,4)
+const generatingPolynomial = "1011";
 // настройка для передачи статических файлов (__dirname - текущая директория)
 // метод join используется для соединения путей с учётом особенностей операционной системы
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 // по корневому запросу отдаем файл index.html из папки ./frontend
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
-})
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/frontend/index.html"));
+});
 
 // описание long polling запроса
-app.get('/long-polling-request', (req, res) => {
-  const MAX_TIMEOUT = 10000; // максимальное время ожидания ответа
-  const MIN_TIMEOUT = 1000; // минимальное время ожидания
-  const MAX_VALUE = 1000; // максимальное значение случайного числа
-  const MIN_VALUE = 1; // минимальное значение случайного числа
+app.get("/long-polling-request", (req, res) => {
+	// выбирается случайное время ожидания из отрезка [1000, 10 000]
+	const timeout = Math.round(
+		(MAX_TIMEOUT - MIN_TIMEOUT) * Math.random() + MIN_TIMEOUT
+	);
 
-  // выбирается случайное время ожидания из отрезка [1000, 10000]
-  const timeout = Math.round((MAX_TIMEOUT - MIN_TIMEOUT) * Math.random() + MIN_TIMEOUT);
+	// выбирается случайное число из отрезка [1, 15], представляется в двоичном виде
+	const value = Math.round((MAX_VALUE - MIN_VALUE) * Math.random() + MIN_VALUE);
+	let original = value.toString(2);
 
-  // выбирается случайное число из отрезка [1, 1000]
-  const value = Math.round((MAX_VALUE - MIN_VALUE) * Math.random() + MIN_VALUE);
+	while (original.length < POLY_LEN) {
+		original = "0" + original;
+	}
+	let encoded = original + "000";
+  // остаток от деления на образующий полином
+	let remainder = getDividingRemainder(encoded);
+	while (remainder.length < ENCODED_POLY_LEN - POLY_LEN) {
+    // добавляем нулевые биты, чтобы добиться фиксированной длины строки - 3 символа
+		remainder = "0" + remainder;
+	}
+  // 4 бита сообщения + 3 бита контрольных на основе остатка
+	encoded = (parseInt(encoded, 2) + parseInt(remainder, 2)).toString(2);
+	while (encoded.length < ENCODED_POLY_LEN) {
+		encoded = "0" + encoded;
+	}
+	let corrupted;
 
-  setTimeout(() => {
-    // если случайное время ожидания превысило максимальное для сервера, то отдаем ошибку
-    if (timeout > MAX_RESPONSE_TIMEOUT) {
-      res.sendStatus(502);
-    } else { // иначе отправляем объект
-      res.send({value, timeout: (timeout / 1000).toPrecision(2)});
-    }
-  }, Math.min(7000, MAX_RESPONSE_TIMEOUT))
-})
+	// рандомим количество ошибок (от 0 до 2)
+	let errorCount = Math.round(Math.random() * 2);
+
+	if (errorCount === 1) {
+		corrupted = makeOneErr(corrupted);
+	}
+
+	if (errorCount === 2) {
+		corrupted = makeTwoErr(corrupted);
+	}
+
+	setTimeout(() => {
+		if (timeout > MAX_RESPONSE_TIMEOUT) {
+			res.sendStatus(502);
+		} else {
+			res.send({
+				original_polynomial: original,
+				encoded_polynomial: encoded,
+				corrupted_polynomial: corrupted,
+				error_count: errorCount,
+			});
+		}
+	}, Math.min(timeout, MAX_RESPONSE_TIMEOUT));
+});
 
 // запуск сервера приложения
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
-})
+	console.log(`Server started at http://localhost:${PORT}`);
+});
+
+const makeOneErr = (corrupted) => {
+	// рандомим позицию ошибки
+	let errIndex = Math.round(Math.random() * (ENCODED_POLY_LEN - 1));
+
+	// строки в js неизменяемы, нужно перегонять в массив
+	const encodedArr = corrupted.split("");
+
+	// меняем бит на противоположный
+	encodedArr[errIndex] = encodedArr[errIndex] === "0" ? "1" : "0";
+
+	// собираем обратно в строку
+	corrupted = encodedArr.join("");
+
+	return corrupted;
+};
+
+const makeTwoErr = (corrupted) => {
+	let errIndex_1 = Math.round(Math.random() * (ENCODED_POLY_LEN - 1));
+	let errIndex_2 = Math.round(Math.random() * (ENCODED_POLY_LEN - 1));
+	// в цикле проверяем, чтобы позиции ошибок были разные
+	for (; errIndex_2 === errIndex_1;) {
+		errIndex_2 = Math.round(Math.random() * (ENCODED_POLY_LEN - 1));
+	}
+
+	const encodedArr = corrupted.split("");
+
+	encodedArr[errIndex_1] = encodedArr[errIndex_1] === "0" ? "1" : "0";
+	encodedArr[errIndex_2] = encodedArr[errIndex_2] === "0" ? "1" : "0";
+
+	corrupted = encodedArr.join("");
+
+	return corrupted;
+};
+
+const getDividingRemainder = (polynomial) => {
+	let indexEnd = generatingPolynomial.length - 1;
+	let currentDigit = polynomial.slice(0, indexEnd + 1);
+	let remainder;
+
+	while (indexEnd < polynomial.length) {
+		remainder = (parseInt(currentDigit, 2) ^ parseInt(generatingPolynomial, 2)).toString(2);
+		currentDigit = remainder;
+
+		if (++indexEnd < polynomial.length) {
+			while (
+				indexEnd < polynomial.length &&
+				currentDigit.length < generatingPolynomial.length
+			) {
+				currentDigit += polynomial[indexEnd++];
+			}
+
+			if (currentDigit.length < generatingPolynomial.length) {
+				remainder = currentDigit;
+			} else {
+				indexEnd--;
+			}
+		}
+	}
+
+	if (remainder.length > generatingPolynomial.length - 1) {
+		remainder = (parseInt(currentDigit, 2) ^ parseInt(generatingPolynomial, 2)).toString(2);
+	}
+
+	return remainder;
+};
